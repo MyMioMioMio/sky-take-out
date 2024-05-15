57198f8d450116535928a24238182c4ba598a004
MYMioMio <1030708179@qq.com>
2024-05-15 20:42:32 +0800

    (非业务代码)实现mp公共字段填充功能(补充)

--- sky-server/src/main/java/com/sky/autofill/MybatisPlusAutoFill.java
+++ sky-server/src/main/java/com/sky/autofill/MybatisPlusAutoFill.java
@@ -1,6 +1,7 @@
 package com.sky.autofill;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.sky.constant.AutoFillConstant;
 import com.sky.context.BaseContext;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
@@ -17,17 +18,17 @@ public class MybatisPlusAutoFill implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("公共字段自动填充:{}", metaObject);
-        metaObject.setValue("createTime", LocalDateTime.now());
-        metaObject.setValue("createUser", BaseContext.getCurrentId());
+        metaObject.setValue(AutoFillConstant.CREATE_TIME, LocalDateTime.now());
+        metaObject.setValue(AutoFillConstant.CREATE_USER, BaseContext.getCurrentId());
 
-        metaObject.setValue("updateTime", LocalDateTime.now());
-        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+        metaObject.setValue(AutoFillConstant.UPDATE_TIME, LocalDateTime.now());
+        metaObject.setValue(AutoFillConstant.UPDATE_USER, BaseContext.getCurrentId());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("公共字段自动填充:{}", metaObject);
-        metaObject.setValue("updateTime", LocalDateTime.now());
-        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+        metaObject.setValue(AutoFillConstant.UPDATE_TIME, LocalDateTime.now());
+        metaObject.setValue(AutoFillConstant.UPDATE_USER, BaseContext.getCurrentId());
     }
 }
