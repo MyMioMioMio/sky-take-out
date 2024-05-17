b8d91cea7fd2268444149349fcb618dea6e039f4
MYMioMio <1030708179@qq.com>
2024-05-17 22:01:51 +0800

    菜品分页查询业务代码

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -11,4 +13,11 @@ public interface DishService {
      * @param dishDTO
      */
     void addDish(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult getPage(DishPageQueryDTO dishPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,10 +1,15 @@
 package com.sky.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +44,24 @@ public class DishServiceImpl implements DishService {
             dishFlavorMapper.insertAll(flavors);
         }
 
+    }
+
+    @Override
+    public PageResult getPage(DishPageQueryDTO dishPageQueryDTO) {
+        //封装分页信息
+        IPage<Dish> page = new Page<>(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());
+        //拆分条件
+        String name = dishPageQueryDTO.getName();
+        Integer categoryId = dishPageQueryDTO.getCategoryId();
+        Integer status = dishPageQueryDTO.getStatus();
+        //设置查询条件
+        LambdaQueryWrapper<Dish> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(categoryId != null, Dish::getCategoryId, categoryId)
+                .eq(status != null, Dish::getStatus, status)
+                .like(name != null && !name.isEmpty(), Dish::getName, name)
+                .orderByDesc(Dish::getCreateTime);
+        //分页查询
+        dishMapper.selectPage(page, wrapper);
+        return new PageResult(page.getTotal(), page.getRecords());
     }
 }
