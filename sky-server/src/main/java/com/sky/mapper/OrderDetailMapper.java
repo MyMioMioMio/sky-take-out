fbb14d8c4126a71b60fb651896d11d438787cd22
MYMioMio <1030708179@qq.com>
2024-06-12 11:34:59 +0800

    C端-订单接口-用户下单业务代码的开发

---
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -0,0 +1,16 @@
+package com.sky.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sky.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.ArrayList;
+
+@Mapper
+public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
+    /**
+     * 批量插入
+     * @param orderDetails
+     */
+    void insertBatch(ArrayList<OrderDetail> orderDetails);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -0,0 +1,89 @@
+package com.sky.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.sky.constant.MessageConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.AddressBook;
+import com.sky.entity.OrderDetail;
+import com.sky.entity.Orders;
+import com.sky.entity.ShoppingCart;
+import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.ShoppingCartBusinessException;
+import com.sky.mapper.AddressBookMapper;
+import com.sky.mapper.OrderDetailMapper;
+import com.sky.mapper.OrderMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    AddressBookMapper addressBookMapper;
+
+    @Autowired
+    OrderMapper orderMapper;
+
+    @Autowired
+    OrderDetailMapper orderDetailMapper;
+
+    @Override
+    public OrderSubmitVO submit(OrdersSubmitDTO ordersSubmitDTO) {
+        // 判断有无地址
+        AddressBook addressBook = addressBookMapper.selectById(ordersSubmitDTO.getAddressBookId());
+        if (addressBook == null) {
+            throw new AddressBookBusinessException(MessageConstant.ADDRESS_BOOK_IS_NULL);
+        }
+        // 判断购物车是否为空
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.selectList(new LambdaQueryWrapper<ShoppingCart>().eq(ShoppingCart::getUserId, BaseContext.getCurrentId()));
+        if (shoppingCartList == null || shoppingCartList.isEmpty()) {
+            throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
+        }
+        // 插入一条订单信息
+        // 封装信息
+        Orders orders = new Orders();
+        BeanUtils.copyProperties(ordersSubmitDTO, orders);
+        orders.setNumber(UUID.randomUUID().toString() + "-" + BaseContext.getCurrentId());
+        orders.setStatus(Orders.PENDING_PAYMENT);
+        orders.setUserId(BaseContext.getCurrentId());
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setPayStatus(Orders.UN_PAID);
+        orders.setPhone(addressBook.getPhone());
+        orders.setConsignee(addressBook.getConsignee());
+        orderMapper.insert(orders);
+        // 插入多条订单详情信息
+        // 封装订单详情
+        ArrayList<OrderDetail> orderDetails = new ArrayList<>();
+        shoppingCartList.forEach(shoppingCart -> {
+            OrderDetail orderDetail = new OrderDetail();
+            BeanUtils.copyProperties(shoppingCart, orderDetail);
+            orderDetail.setOrderId(orders.getId());
+            orderDetails.add(orderDetail);
+        });
+        orderDetailMapper.insertBatch(orderDetails);
+        // 清空购物车
+        shoppingCartMapper.delete(new LambdaQueryWrapper<ShoppingCart>().eq(ShoppingCart::getUserId, BaseContext.getCurrentId()));
+
+        //封装VO
+        OrderSubmitVO orderSubmitVO = OrderSubmitVO.builder()
+                .id(orders.getId())
+                .orderTime(orders.getOrderTime())
+                .orderAmount(orders.getAmount())
+                .orderNumber(orders.getNumber())
+                .build();
+        return orderSubmitVO;
+    }
+}
